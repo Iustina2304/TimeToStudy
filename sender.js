f19d7e76cedf4544b20d49a2c664731e9cfd1395
anaacsinte <82666641+anaacsinte@users.noreply.github.com>
2021-04-26 18:56:44 +0300

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,31 @@
+const webSocket = new WebSocket("ws://351.0.0.1:3000")
+
+function sendUsername() {
+    username = document.getElementById("username-input").value
+    sendData({
+        type: "store_user",
+    })
+}
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+let localStream
+function startCall() {
+    document.getElementById("video-call-div").style.display = "inline"
+    navigator.getUserMedia({
+        video: {
+            frameRate : 24,
+            width : {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) =>{
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream;
+    }, (error) => {
+        console.log(error)
+    })
+}
